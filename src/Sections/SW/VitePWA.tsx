db1de5d9c6cf3c60a746a43dd094491776c3ea70
Alex Schulz <schubz94@gmail.com>
2024-07-09 20:37:27 +0200

    cnisjbvri

--- src/Sections/SW/VitePWA.tsx
+++ src/Sections/SW/VitePWA.tsx
@@ -1,4 +1,4 @@
-import { FaArrowRightLong, FaArrowTurnDown } from "react-icons/fa6"
+import { FaArrowRightLong } from "react-icons/fa6"
 import { Link } from "react-router-dom"
 
 const VitePWA = () => {

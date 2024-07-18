18520d6b4dc340dcce52ce5736005267fca3fecd
Alex <schubz94@gmail.com>
2024-07-18 22:54:20 +0200

    added ScrollToTop and added new Screenshot in correct size

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,12 @@ import PWABadge from './PWABadge.tsx';
 import './App.css';
 import SW from "./Pages/SW.tsx";
 import PWA from "./Pages/PWA.tsx";
+import ScrollToTop from './Components/ScrollToTop.tsx';
 
 function App() {
     return (
         <Router>
+            <ScrollToTop />
             <Routes>
                 <Route path="/" element={<SW />} />
                 <Route path="/pwa" element={<PWA />} />
